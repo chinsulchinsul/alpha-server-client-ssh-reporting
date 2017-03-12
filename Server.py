12d5c43241136745492e7772eeeea2bab4ddd457
Appu A S <appu.as@livestream.com>
2017-03-12 21:00:16 +0530

    Print statements

--- Server.py
+++ Server.py
@@ -1,24 +1,22 @@
 #!/usr/bin/env python
 import mysql.connector
 
-def initiate_db_connection(user_name, password, database_name, host_name):
+def initiate_db_connection(user_name, password, host_name):
     try:
         cnx = mysql.connector.connect(user=user_name,
-                                    database=database_name,
                                     password=password,
                                     host=host_name)
+        print "MySQL connection succeeded"
+        return cnx
     except mysql.connector.Error as err:
-        if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-            print("Something is wrong with your user name or password")
-        elif err.errno == errorcode.ER_BAD_DB_ERROR:
-            print("Database does not exist")
-        else:
-            print(err)
-    return cnx
+        print "MySQL connection error %s" %str(err)
+    
+
 
 def close_db_connection(cnx):
     try:
         cnx.close()
+        print "Database connection closed successfully."
     except mysql.connector.Error as err:
         print "Error closing db connection %s" %str(err)
 
@@ -26,16 +24,26 @@ def db_execute(cnx,sql):
     try:
         cursor = cnx.cursor()
         cursor.execute(sql)
+        print "Query executed successfully."
     except mysql.connector.Error as err:
         print "Error executing query %s" %str(err)
+
 def main():
     db_user = 'root'
     db_password = 'strong_password'
     db_name = 'ssh_access_reports'
     db_host = '127.0.0.1'
 
-    connection = initiate_db_connection(db_user,db_password,db_name,db_host)
+    print "Connecting to mysql"
+    connection = initiate_db_connection(db_user,db_password,db_host)
 
+    print "Creating databse if not existing"
     create_db_query='CREATE DATABASE IF NOT EXISTS %s' %db_name
+
+    db_execute(connection,create_db_query)
+
+    print "Closing database connection"
+    close_db_connection(connection)
+
 if __name__ == "__main__":
     main()
